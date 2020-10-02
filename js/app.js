ee87fbe0e45332b54576179fe26f6ccad221ae7d
Lucas Simões <lucassimoes@gruposgapetro.com.br>
2020-10-01 23:38:32 -0300

    Correções e melhorias

--- js/app.js
+++ js/app.js
@@ -23,33 +23,34 @@ document.addEventListener("DOMContentLoaded", () => {
         elDivFlipCardFront.classList.add('flip-card-front');
 
         elDivFlipCardBack = document.createElement('div');
-        elDivFlipCardBack.classList.add('flip-card-back');     
+        elDivFlipCardBack.classList.add('flip-card-back'); 
+        
+        elTextFrontId = document.createElement('h2');
+        elTextFrontId.classList.add('card-number'); 
+        elTextFrontId.innerText = item.codigo;
         
         elImgFront = document.createElement('img');
-        elImgFront.src = 'img\\img_avatar.png';
+       // elImgFront.src = 'img\\img_avatar.png';
         elImgFront.alt = 'Avatar'
         elImgFront.style = 'width:300px;height:300px;';
 
-        if (item.termo == '') {
+        if (item.termo) {
             elTextBack = document.createElement('p');
-            elTextBack.innerText = item.definicao;
+            elTextBack.innerText = item.texto;
         } else {
             elTextBack = document.createElement('h2');
-            elTextBack.innerText = item.termo;
+            elTextBack.innerText = item.texto;
         }
         
         elDiv.appendChild(elDivFlipCard);
         elDivFlipCard.appendChild(elDivFlipCardInner);
 
-        if (item.termo == '') {
-            elDivFlipCard.id = 'b'+item.codigo;
-        } else {
-            elDivFlipCard.id = 'a'+item.codigo;
-        } 
+        elDivFlipCard.id = item.codigo
 
         elDivFlipCardInner.appendChild(elDivFlipCardFront);
         elDivFlipCardInner.appendChild(elDivFlipCardBack);
         elDivFlipCardFront.appendChild(elImgFront);
+        elDivFlipCardFront.appendChild(elTextFrontId);
         elDivFlipCardBack.appendChild(elTextBack);
 
 
@@ -59,6 +60,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const flipCard = document.querySelectorAll('.flip-card');
     for(var i of flipCard){
+     /*   i.addEventListener('mouseover', function(){              
+            if (listItemParMemoria.find(x => x.codigo == this.id).audiodescricao != '' && this.classList.contains('flip-card-click')) {
+                var audioplayer = document.getElementById('audioplay');
+                audioplayer.src = listItemParMemoria.find(x => x.codigo == this.id).audiodescricao
+                audioplayer.play();
+            }
+        })*/
+
         i.addEventListener('click', function(){
 
             if (!this.classList.contains('finded')) {
@@ -71,7 +80,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 } else {
                     if (idCartaVirada == undefined) {
                         idCartaVirada = this;
-                    } else {
+                    } else {                                             
                         if (idCartaVirada.id == this.id){
                             idCartaVirada = undefined;          
                             flipCount = 0;
@@ -79,7 +88,7 @@ document.addEventListener("DOMContentLoaded", () => {
                             return;
                         }
 
-                        if (idCartaVirada.id.substring(1) == this.id.substring(1)) {
+                        if (listItemParMemoria.find(x => x.codigo == idCartaVirada.id).par == listItemParMemoria.find(x => x.codigo == this.id).par){
                             flipCount = 0;
                             idCartaVirada.classList.add('flip-card-click');
                             idCartaVirada.classList.add('finded');
@@ -89,7 +98,15 @@ document.addEventListener("DOMContentLoaded", () => {
                         idCartaVirada = undefined;                
                     }  
                 } 
-            }                 
+            } 
+            
+            if (listItemParMemoria.find(x => x.codigo == this.id).audiodescricao != '' && this.classList.contains('flip-card-click')) {
+                var audioplayer = document.getElementById('audioplay');
+                audioplayer.src = listItemParMemoria.find(x => x.codigo == this.id).audiodescricao
+                audioplayer.play();
+            }            
+
+
         })
     }
 });
@@ -115,134 +132,235 @@ function shuffle(array) {
 
 function itemParMemoria() {
     this.codigo = 0;
-    this.termo = '';
-    this.definicao = '';
+    this.par = 0;
+    this.termo = false;
+    this.definicao = false;
+    this.texto = '';
+    this.audiodescricao = '';
     this.encontrou = false;
+    this.virou = false;
 }
 
 var listItemParMemoria = [];
 
 function preencheListItemParMemoria () {
-    //TERMO
     var item = new itemParMemoria();
     item.codigo = 1;
-    item.termo = 'Princípio do SUS: INTEGRALIDADE';
-    item.definicao = '';
-    listItemParMemoria.push(item);  
-	
+    item.par = 1;
+    item.termo = true;
+    item.definicao = false;
+    item.texto = 'Princípio do SUS: INTEGRALIDADE';
+    item.audiodescricao = 'mp3/termo1.mp3';
+    item.encontrou = false;
+    item.virou = false;
+    listItemParMemoria.push(item);
+    
     var item = new itemParMemoria();
     item.codigo = 2;
-    item.termo = 'Princípio do SUS: EQUIDADE';
-    item.definicao = '';
-    listItemParMemoria.push(item);  	
-	
+    item.par = 1;
+    item.termo = false;
+    item.definicao = true;
+    item.texto = 'Princípio que considera as pessoas como um todo, atendendo a todas as suas necessidades.';
+    item.audiodescricao = 'mp3/definicao1.mp3';
+    item.encontrou = false;
+    item.virou = false;
+    listItemParMemoria.push(item);
+    
     var item = new itemParMemoria();
     item.codigo = 3;
-    item.termo = 'Princípio do SUS: UNIVERSALIZAÇÃO';
-    item.definicao = '';
-    listItemParMemoria.push(item);  		
-	
+    item.par = 2;
+    item.termo = true;
+    item.definicao = false;
+    item.texto = 'Princípio do SUS: EQUIDADE';
+    item.audiodescricao = 'mp3/termo2.mp3';
+    item.encontrou = false;
+    item.virou = false;
+    listItemParMemoria.push(item);
+    
     var item = new itemParMemoria();
     item.codigo = 4;
-    item.termo = 'Princípio Organizativo: REGIONALIZAÇÃO';
-    item.definicao = '';
-    listItemParMemoria.push(item);  
-	
+    item.par = 2;
+    item.termo = false;
+    item.definicao = true;
+    item.texto = 'Princípio que tem como objetivo diminuir as desigualdades.';
+    item.audiodescricao = 'mp3/definicao2.mp3';
+    item.encontrou = false;
+    item.virou = false;
+    listItemParMemoria.push(item);
+    
     var item = new itemParMemoria();
     item.codigo = 5;
-    item.termo = 'Princípio Organizativo: HIERARQUIZAÇÃO';
-    item.definicao = '';
-    listItemParMemoria.push(item);  
-	
+    item.par = 3;
+    item.termo = true;
+    item.definicao = false;
+    item.texto = 'Princípio do SUS: UNIVERSALIZAÇÃO';
+    item.audiodescricao = 'mp3/termo3.mp3';
+    item.encontrou = false;
+    item.virou = false;
+    listItemParMemoria.push(item);
+    
     var item = new itemParMemoria();
     item.codigo = 6;
-    item.termo = 'Princípio Organizativo: DESCENTRALIZAÇÃO';
-    item.definicao = '';
+    item.par = 3;
+    item.termo = false;
+    item.definicao = true;
+    item.texto = 'Princípio que visa que a saúde é um direito de cidadania de todas as pessoas e cabe ao Estado assegurar este direito, sendo que o acesso às ações e serviços deve ser garantido a todas as pessoas, independentemente de sexo, raça, ocupação ou outras características sociais ou pessoais.';
+    item.audiodescricao = 'mp3/definicao3.mp3';
+    item.encontrou = false;
+    item.virou = false;
     listItemParMemoria.push(item);
-	
-/*     var item = new itemParMemoria();
+    
+    var item = new itemParMemoria();
     item.codigo = 7;
-    item.termo = 'Princípio Organizativo: POPULAR';
-    item.definicao = '';
-    listItemParMemoria.push(item);	
-	
+    item.par = 4;
+    item.termo = true;
+    item.definicao = false;
+    item.texto = 'Princípio Organizativo: REGIONALIZAÇÃO';
+    item.audiodescricao = 'mp3/termo4.mp3';
+    item.encontrou = false;
+    item.virou = false;
+    listItemParMemoria.push(item);
+    
     var item = new itemParMemoria();
     item.codigo = 8;
-    item.termo = 'Responsabilidade da União';
-    item.definicao = '';
-    listItemParMemoria.push(item);		
-
-    var item = new itemParMemoria();
-    item.codigo = 9;
-    item.termo = 'Responsabilidade dos Estados';
-    item.definicao = '';
-    listItemParMemoria.push(item);	
-	 
-    var item = new itemParMemoria();
-    item.codigo = 10;
-    item.termo = 'Responsabilidades do Município';
-    item.definicao = '';
-    listItemParMemoria.push(item); */
-
-    //DEFINIÇÃO
-    var item = new itemParMemoria();
-    item.codigo = 1;
-    item.termo = '';
-    item.definicao = 'Princípio que considera as pessoas como um todo, atendendo a todas as suas necessidades.';
-    listItemParMemoria.push(item);  
-	
-    var item = new itemParMemoria();
-    item.codigo = 2;
-    item.termo = '';
-    item.definicao = 'Princípio que tem como objetivo diminuir as desigualdades.';
-    listItemParMemoria.push(item);  	
-	
-    var item = new itemParMemoria();
-    item.codigo = 3;
-    item.termo = '';
-    item.definicao = 'Princípio que visa que a saúde é um direito de cidadania de todas as pessoas e cabe ao Estado assegurar este direito, sendo que o acesso às ações e serviços deve ser garantido a todas as pessoas, independentemente de sexo, raça, ocupação ou outras características sociais ou pessoais.';
-    listItemParMemoria.push(item);  		
-	
-    var item = new itemParMemoria();
-    item.codigo = 4;
-    item.termo = '';
-    item.definicao = 'É um processo de articulação entre os serviços que já existem, visando o comando unificado dos mesmos.';
-    listItemParMemoria.push(item);  
-	
-    var item = new itemParMemoria();
-    item.codigo = 5;
-    item.termo = '';
-    item.definicao = 'Deve proceder à divisão de níveis de atenção e garantir formas de acesso a serviços que façam parte da complexidade requerida pelo caso, nos limites dos recursos disponíveis numa dada região.';
-    listItemParMemoria.push(item);  
-	
-    var item = new itemParMemoria();
-    item.codigo = 6;
-    item.termo = '';
-    item.definicao = 'Visa redistribuir poder e responsabilidade entre os três níveis de governo.';
+    item.par = 4;
+    item.termo = false;
+    item.definicao = true;
+    item.texto = 'É um processo de articulação entre os serviços que já existem, visando o comando unificado dos mesmos.';
+    item.audiodescricao = 'mp3/definicao4.mp3';
+    item.encontrou = false;
+    item.virou = false;
     listItemParMemoria.push(item);
-	
-/*     var item = new itemParMemoria();
-    item.codigo = 7;
-    item.termo = '';
-    item.definicao = 'Nesse princípio a sociedade deve participar no dia-a-dia do sistema. Para isto, devem ser criados os Conselhos e as Conferências de Saúde, que visam formular estratégias, controlar e avaliar a execução da política de saúde.';
-    listItemParMemoria.push(item);	
-	
-    var item = new itemParMemoria();
-    item.codigo = 8;
-    item.termo = '';
-    item.definicao = 'O principal financiador da rede pública de saúde. Historicamente, o Ministério da Saúde aplica metade de todos os recursos gastos no país em saúde pública em todo o Brasil, e estados e municípios, em geral, contribuem com a outra metade dos recursos';
-    listItemParMemoria.push(item);		
-
+    
     var item = new itemParMemoria();
     item.codigo = 9;
-    item.termo = '';
-    item.definicao = 'Possuem secretarias específicas para a gestão de saúde. O gestor deve aplicar recursos próprios, inclusive nos municípios, e os repassados pela União. Além de ser um dos parceiros para a aplicação de políticas nacionais de saúde, formula suas próprias políticas de saúde. Ele coordena e planeja o SUS em nível estadual, respeitando a normatização federal.';
-    listItemParMemoria.push(item);	
-	 
+    item.par = 5;
+    item.termo = true;
+    item.definicao = false;
+    item.texto = 'Princípio Organizativo: HIERARQUIZAÇÃO';
+    item.audiodescricao = 'mp3/termo5.mp3';
+    item.encontrou = false;
+    item.virou = false;
+    listItemParMemoria.push(item);
+    
     var item = new itemParMemoria();
     item.codigo = 10;
-    item.termo = '';
-    item.definicao = 'Responsáveis pela execução das ações e serviços de saúde no âmbito do seu território.  O gestor deve aplicar recursos próprios e os repassados pela União e pelo estado. O município formula suas próprias políticas de saúde e também é um dos parceiros para a aplicação de políticas nacionais e estaduais de saúde.';
-    listItemParMemoria.push(item); */
-
+    item.par = 5;
+    item.termo = false;
+    item.definicao = true;
+    item.texto = 'Deve proceder à divisão de níveis de atenção e garantir formas de acesso a serviços que façam parte da complexidade requerida pelo caso, nos limites dos recursos disponíveis numa dada região.';
+    item.audiodescricao = 'mp3/definicao5.mp3';
+    item.encontrou = false;
+    item.virou = false;
+    listItemParMemoria.push(item);
+    
+    var item = new itemParMemoria();
+    item.codigo = 11;
+    item.par = 6;
+    item.termo = true;
+    item.definicao = false;
+    item.texto = 'Princípio Organizativo: DESCENTRALIZAÇÃO';
+    item.audiodescricao = 'mp3/termo6.mp3';
+    item.encontrou = false;
+    item.virou = false;
+    listItemParMemoria.push(item);
+    
+    var item = new itemParMemoria();
+    item.codigo = 12;
+    item.par = 6;
+    item.termo = false;
+    item.definicao = true;
+    item.texto = 'Visa redistribuir poder e responsabilidade entre os três níveis de governo.';
+    item.audiodescricao = 'mp3/definicao6.mp3';
+    item.encontrou = false;
+    item.virou = false;
+    listItemParMemoria.push(item);
+    
+    var item = new itemParMemoria();
+    item.codigo = 13;
+    item.par = 7;
+    item.termo = true;
+    item.definicao = false;
+    item.texto = 'Princípio Organizativo: POPULAR';
+    item.audiodescricao = 'mp3/termo7.mp3';
+    item.encontrou = false;
+    item.virou = false;
+    listItemParMemoria.push(item);
+    
+    var item = new itemParMemoria();
+    item.codigo = 14;
+    item.par = 7;
+    item.termo = false;
+    item.definicao = true;
+    item.texto = 'Nesse princípio a sociedade deve participar no dia-a-dia do sistema. Para isto, devem ser criados os Conselhos e as Conferências de Saúde, que visam formular estratégias, controlar e avaliar a execução da política de saúde.';
+    item.audiodescricao = 'mp3/definicao7.mp3';
+    item.encontrou = false;
+    item.virou = false;
+    listItemParMemoria.push(item);
+    
+    var item = new itemParMemoria();
+    item.codigo = 15;
+    item.par = 8;
+    item.termo = true;
+    item.definicao = false;
+    item.texto = 'Responsabilidade da União';
+    item.audiodescricao = 'mp3/termo8.mp3';
+    item.encontrou = false;
+    item.virou = false;
+    listItemParMemoria.push(item);
+    
+    var item = new itemParMemoria();
+    item.codigo = 16;
+    item.par = 8;
+    item.termo = false;
+    item.definicao = true;
+    item.texto = 'O principal financiador da rede pública de saúde. Historicamente, o Ministério da Saúde aplica metade de todos os recursos gastos no país em saúde pública em todo o Brasil, e estados e municípios, em geral, contribuem com a outra metade dos recursos';
+    item.audiodescricao = 'mp3/definicao8.mp3';
+    item.encontrou = false;
+    item.virou = false;
+    listItemParMemoria.push(item);
+    
+    var item = new itemParMemoria();
+    item.codigo = 17;
+    item.par = 9;
+    item.termo = true;
+    item.definicao = false;
+    item.texto = 'Responsabilidade dos Estados';
+    item.audiodescricao = 'mp3/termo9.mp3';
+    item.encontrou = false;
+    item.virou = false;
+    listItemParMemoria.push(item);
+    
+    var item = new itemParMemoria();
+    item.codigo = 18;
+    item.par = 9;
+    item.termo = false;
+    item.definicao = true;
+    item.texto = 'Possuem secretarias específicas para a gestão de saúde. O gestor deve aplicar recursos próprios, inclusive nos municípios, e os repassados pela União. Além de ser um dos parceiros para a aplicação de políticas nacionais de saúde, formula suas próprias políticas de saúde. Ele coordena e planeja o SUS em nível estadual, respeitando a normatização federal.';
+    item.audiodescricao = 'mp3/definicao9.mp3';
+    item.encontrou = false;
+    item.virou = false;
+    listItemParMemoria.push(item);
+    
+    var item = new itemParMemoria();
+    item.codigo = 19;
+    item.par = 10;
+    item.termo = true;
+    item.definicao = false;
+    item.texto = 'Responsabilidades do Município';
+    item.audiodescricao = 'mp3/termo10.mp3';
+    item.encontrou = false;
+    item.virou = false;
+    listItemParMemoria.push(item);
+    
+    var item = new itemParMemoria();
+    item.codigo = 20;
+    item.par = 10;
+    item.termo = false;
+    item.definicao = true;
+    item.texto = 'Responsáveis pela execução das ações e serviços de saúde no âmbito do seu território.  O gestor deve aplicar recursos próprios e os repassados pela União e pelo estado. O município formula suas próprias políticas de saúde e também é um dos parceiros para a aplicação de políticas nacionais e estaduais de saúde.';
+    item.audiodescricao = 'mp3/definicao10.mp3';
+    item.encontrou = false;
+    item.virou = false;
+    listItemParMemoria.push(item);    
 }
